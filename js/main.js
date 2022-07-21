0378cc01989f1bf8464f611ff6629fb06a576f1e
Dulce Herrera <dulceherrera11@gmail.com>
2022-07-21 04:37:30 +0000

    Ensure that refreshing the pages shows the same view as before refreshing.

--- js/main.js
+++ js/main.js
@@ -3,12 +3,13 @@ var $form = document.querySelector('.form');
 var $img = document.querySelector('#img-entry');
 var $title = document.querySelector('#title');
 var $notes = document.querySelector('#notes');
+var $noEntries = document.querySelector('.no-entries');
 
-$form.addEventListener('input', function (event) {
-  if (event.target.id === 'photourl') {
-    $img.setAttribute('src', event.target.value);
-  }
-});
+function updateImg(event) {
+  $img.setAttribute('src', $photoUrl.value);
+}
+
+$photoUrl.addEventListener('input', updateImg);
 
 $form.addEventListener('submit', function (event) {
   event.preventDefault();
@@ -25,6 +26,14 @@ $form.addEventListener('submit', function (event) {
   $img.setAttribute('src', './images/placeholder-image-square.jpg');
   $form.reset();
 
+  $entries.className = 'view entries container';
+  $entryForm.className = 'view entry-form hidden container';
+  data.view = 'entries';
+
+  var $ul = document.querySelector('ul');
+  var $newEntry = createList(newObj);
+  $ul.prepend($newEntry);
+  $noEntries.className = 'hidden';
 });
 
 function createList(data) {
@@ -58,9 +67,59 @@ function createList(data) {
 
 window.addEventListener('DOMContentLoaded', function (event) {
   var $ul = document.createElement('ul');
+
   for (var i = 0; i < data.entries.length; i++) {
     var result = createList(data.entries[i]);
     $ul.appendChild(result);
   }
+});
+
+var $buttonNew = document.querySelector('.button-new');
+var $entryTab = document.querySelector('.entry-tab');
+var $entries = document.querySelector('.entries');
+var $entryForm = document.querySelector('.entry-form');
+var $formTab = document.querySelector('.form-tab');
+
+$buttonNew.addEventListener('click', function (event) {
+  if ($entryForm !== '') {
+    $entries.className = 'entries hidden';
+    $entryForm.className = 'entry-form container';
+    data.view = 'entry-form';
+  }
+});
+
+$entryTab.addEventListener('click', function (event) {
+  if ($entryForm !== '') {
+    $entries.className = 'entries hidden container';
+    $entryForm.className = 'entry-form container';
+    data.view = 'entry-form';
+  }
+});
+
+$entryTab.addEventListener('click', function (event) {
+  if ($entries !== '') {
+    $entryForm.className = 'entry-form hidden';
+    $entries.className = 'entries container';
+    data.view = 'entries';
+  } else {
+    return 'No entries have been recorded';
+  }
+});
+
+$formTab.addEventListener('click', function (event) {
+  if ($entryForm !== '') {
+    $entries.className = 'entries hidden';
+    $entryForm.className = 'entry-form container';
+    data.view = 'entry-form';
+  }
+});
+
+if (data.view === 'entry-form') {
+  $entries.className = 'view entries hidden';
+  $entryForm.className = 'view entry-form container';
+  data.view = 'entry-form';
+} else {
+  $entries.className = 'view entries container';
+  $entryForm.className = 'view entry-form hidden container';
+  data.view = 'entries';
 }
-);
